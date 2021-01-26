a77a91f517f3bd496aacaeb868805d20adc0ebe8
ARATA Mizuki <minorinoki@gmail.com>
2021-01-26 14:24:38 +0900

    Add more variants

--- benchmark/Benchmark.hs
+++ benchmark/Benchmark.hs
@@ -1,35 +1,41 @@
 {-# LANGUAGE NumericUnderscores #-}
 import Numeric.Floating.IEEE (fromIntegerTiesToEven)
 import Gauge.Main
-import IntegerToFloat (integerToFloat, integerToDouble, integerToBinaryFloat)
+import IntegerToFloat -- (integerToFloat, integerToDouble, integerToBinaryFloat)
 
 main :: IO ()
 main = defaultMain
   [ bgroup "Float"
     [ bgroup label $ map ($ arg)
-      [ bench "fromInteger" . whnf (fromInteger :: Integer -> Float)
-      , bench "fromRational" . whnf (fromRational . fromInteger :: Integer -> Float)
-      , bench "integerToFloat" . whnf integerToFloat
-      , bench "fromIntegerTiesToEven (fp-ieee)" . whnf (fromIntegerTiesToEven :: Integer -> Float)
+      [
+        -- bench "fromInteger" . whnf (fromInteger :: Integer -> Float)
+      -- , bench "fromRational" . whnf (fromRational . fromInteger :: Integer -> Float)
+        bench "integerToFloat" . whnf integerToFloat
+      , bench "integerToFloatS" . whnf integerToFloatS
+      -- , bench "fromIntegerTiesToEven (fp-ieee)" . whnf (fromIntegerTiesToEven :: Integer -> Float)
       , bench "integerToBinaryFloat" . whnf (integerToBinaryFloat :: Integer -> Float)
+      , bench "integerToBinaryFloatX" . whnf (integerToBinaryFloatX :: Integer -> Float)
+      -- , bench "integerToBinaryFloatS" . whnf (integerToBinaryFloatS :: Integer -> Float)
       ]
     | (label,arg) <- [("small", 42)
-                     ,("negative small", -42)
-                     ,("large", 2^128 - 1)
+                     ,("medium", - 2^60 - 1)
                      ,("large", 2^128 - 1)
                      ,("negative large", -(2^128-1))
                      ]
     ]
   , bgroup "Double"
     [ bgroup label $ map ($ arg)
-      [ bench "fromInteger" . whnf (fromInteger :: Integer -> Double)
-      , bench "fromRational" . whnf (fromRational . fromInteger :: Integer -> Double)
-      , bench "integerToDouble" . whnf integerToDouble
-      , bench "fromIntegerTiesToEven (fp-ieee)" . whnf (fromIntegerTiesToEven :: Integer -> Double)
+      [ -- bench "fromInteger" . whnf (fromInteger :: Integer -> Double)
+      -- , bench "fromRational" . whnf (fromRational . fromInteger :: Integer -> Double)
+        bench "integerToDouble" . whnf integerToDouble
+      , bench "integerToDoubleS" . whnf integerToDoubleS
+      -- , bench "fromIntegerTiesToEven (fp-ieee)" . whnf (fromIntegerTiesToEven :: Integer -> Double)
       , bench "integerToBinaryFloat" . whnf (integerToBinaryFloat :: Integer -> Double)
+      , bench "integerToBinaryFloatX" . whnf (integerToBinaryFloatX :: Integer -> Double)
+      -- , bench "integerToBinaryFloatS" . whnf (integerToBinaryFloatS :: Integer -> Double)
       ]
     | (label,arg) <- [("small", 42)
-                     ,("negative small", -42)
+                     ,("medium", - 2^60 - 1)
                      ,("large", 2^128 - 1)
                      ,("negative large", -(2^128 - 1))
                      ]
--- src/IntegerToFloat.hs
+++ src/IntegerToFloat.hs
@@ -1,17 +1,21 @@
+{-# LANGUAGE BangPatterns      #-}
 {-# LANGUAGE MagicHash         #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE CPP #-}
+
+#include "MachDeps.h"
+
 module IntegerToFloat where
 import           Data.Bits
-import           GHC.Base        ((<), (==))
-import           GHC.Exts        (Double (D#), Double#, Float (F#), Float#,
-                                  Int (I#), int2Double#, int2Float#,
-                                  negateDouble#, negateFloat#, word2Int#, (-#))
-import           GHC.Float       (RealFloat (encodeFloat, floatDigits),
-                                  floatDigits, negateDouble, negateFloat,
-                                  roundingMode#)
-import           GHC.Num         (negate, (+), (-))
-import           GHC.Num.Integer (Integer (IN, IP, IS), integerLog2#,
-                                  integerSignum#, integerToInt)
+import           GHC.Base           ((<), (==))
+import           GHC.Exts
+import           GHC.Float          (RealFloat (encodeFloat, floatDigits),
+                                     floatDigits, negateDouble, negateFloat,
+                                     roundingMode#)
+import           GHC.Num            (negate, (+), (-))
+import           GHC.Num.Integer    (Integer (IN, IP, IS), integerLog2#,
+                                     integerSignum#, integerToInt)
+import           GHC.Num.Primitives (intEncodeDouble#, wordLog2#)
 
 integerToFloat :: Integer -> Float
 integerToFloat i = F# (integerToFloat# i)
@@ -37,10 +41,32 @@ integerToDouble# (IN bn)  = case integerToBinaryFloat' (IP bn) of
 
 -- Suitable definition for fromInteger
 integerToBinaryFloat :: RealFloat a => Integer -> a
-integerToBinaryFloat i = case integerSignum# i of
+integerToBinaryFloat (IS 0#) = encodeFloat 0 0
+integerToBinaryFloat i@(IS i#) = if isTrue# (i# ># 0#) then
+                                   integerToBinaryFloat' i
+                                 else
+                                   negate (integerToBinaryFloat' (IS (negateInt# i#)))
+integerToBinaryFloat i@(IP _) = integerToBinaryFloat' i
+integerToBinaryFloat (IN bn) = negate (integerToBinaryFloat' (IP bn))
+{-# SPECIALIZE integerToBinaryFloat :: Integer -> Float, Integer -> Double #-}
+
+integerToBinaryFloatX :: RealFloat a => Integer -> a
+integerToBinaryFloatX (IS 0#) = encodeFloat 0 0
+integerToBinaryFloatX (IS i) = if isTrue# (i ># 0#) then
+                                 intToBinaryFloat' i
+                               else
+                                 negate (intToBinaryFloat' (negateInt# i))
+integerToBinaryFloatX i@(IP _) = integerToBinaryFloat' i
+integerToBinaryFloatX (IN bn) = negate (integerToBinaryFloat' (IP bn))
+{-# SPECIALIZE integerToBinaryFloatX :: Integer -> Float, Integer -> Double #-}
+
+{-
+integerToBinaryFloatS :: RealFloat a => Integer -> a
+integerToBinaryFloatS i = case integerSignum# i of
                            0# -> encodeFloat 0 0
                            1# -> integerToBinaryFloat' i
                            _  {- -1# -} -> negate (integerToBinaryFloat' (negate i))
+-}
 
 integerToBinaryFloat' :: RealFloat a
                       => Integer -- ^ Must be @> 0@.
@@ -53,7 +79,7 @@ integerToBinaryFloat' n = result
     result = if k < mantDigs then
                encodeFloat n 0
              else
-               let e@(I# e#) = k - mantDigs + 1
+               let !e@(I# e#) = k - mantDigs + 1
                    q = n `unsafeShiftR` e
                    n' = case roundingMode# n (e# -# 1#) of
                           0# -> q
@@ -63,4 +89,91 @@ integerToBinaryFloat' n = result
                                   q + 1
                           _ {- 2# -} -> q + 1
                in encodeFloat n' e
-{-# SPECIALIZE integerToBinaryFloat :: Integer -> Float, Integer -> Double #-}
+{-# SPECIALIZE integerToBinaryFloat' :: Integer -> Float, Integer -> Double #-}
+
+intToBinaryFloat' :: RealFloat a
+                  => Int# -- ^ Must be @> 0@.
+                  -> a
+intToBinaryFloat' n = result
+  where
+    -- @floatRadix result@ must be 2
+    mantDigs = floatDigits result
+    k = I# (word2Int# (wordLog2# (int2Word# n)))
+    result = if k < mantDigs then
+               encodeFloat (IS n) 0
+             else
+               let !e@(I# e#) = k - mantDigs + 1
+                   !q = n `uncheckedIShiftRL#` e#
+                   !n' = case roundingMode# (IS n) (e# -# 1#) of -- ?
+                           0# -> q
+                           1# -> if isTrue# (q `andI#` 1# ==# 0#) then
+                                   q
+                                 else
+                                   q +# 1#
+                           _ {- 2# -} -> q +# 1#
+               in encodeFloat (IS n') e
+{-# SPECIALIZE intToBinaryFloat' :: Int# -> Float, Int# -> Double #-}
+
+--
+-- Specialized implementations
+--
+
+integerToFloatS :: Integer -> Float
+integerToFloatS i = F# (integerToFloatS# i)
+
+integerToFloatS# :: Integer -> Float#
+integerToFloatS# (IS i)   = int2Float# i
+integerToFloatS# i@(IP _) = integerToFloat' i
+integerToFloatS# (IN bn)  = negateFloat# (integerToFloat' (IP bn))
+{-# NOINLINE integerToFloatS# #-}
+
+integerToFloat' :: Integer -- ^ Must be @>= 2^(WORD_SIZE_IN_BITS-1)@.
+                -> Float#
+integerToFloat' n = result
+  where
+    -- @floatRadix result@ must be 2
+    -- FLT_MANT_DIG must be 23
+    mantDigs = floatDigits (0 :: Float)
+    k = I# (word2Int# (integerLog2# n))
+    -- k >= mantDigs
+    result = let !e@(I# e#) = k - mantDigs + 1
+                 !q@(I# q#) = integerToInt (n `unsafeShiftR` e)
+                 !n' = case roundingMode# n (e# -# 1#) of
+                         0# -> q#
+                         1# -> if q .&. 1 == 0 then
+                                 q#
+                               else
+                                 q# +# 1#
+                         _ {- 2# -} -> q# +# 1#
+             in double2Float# (intEncodeDouble# n' e#)
+
+integerToDoubleS :: Integer -> Double
+integerToDoubleS i = D# (integerToDoubleS# i)
+
+integerToDoubleS# :: Integer -> Double#
+integerToDoubleS# (IS i)   = int2Double# i
+integerToDoubleS# i@(IP _) = integerToDouble' i
+integerToDoubleS# (IN bn)  = negateDouble# (integerToDouble' (IP bn))
+{-# NOINLINE integerToDoubleS# #-}
+
+integerToDouble' :: Integer -- ^ Must be @>= 2^(WORD_SIZE_IN_BITS-1)@.
+                 -> Double#
+integerToDouble' n = result
+  where
+    -- @floatRadix result@ must be 2
+    mantDigs = floatDigits (0 :: Double)
+    k = I# (word2Int# (integerLog2# n))
+#if DBL_MANT_DIGS > WORD_SIZE_IN_BITS
+#error "Unsupported configuration"
+#else
+    result = let !e@(I# e#) = k - mantDigs + 1
+                 !q@(I# q#) = integerToInt (n `unsafeShiftR` e)
+                 !n' = case roundingMode# n (e# -# 1#) of
+                         0# -> q#
+                         1# -> if q .&. 1 == 0 then
+                                 q#
+                               else
+                                 q# +# 1#
+                         _ {- 2# -} -> q# +# 1#
+             in intEncodeDouble# n' e#
+#endif
